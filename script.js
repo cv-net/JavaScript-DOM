c5559cc4392101a6c2470db9962f71a86bc6f7a7
cv-net <khalil.mktg@gmail.com>
2020-12-30 00:23:22 +0000

    button to hide image - part 1 done

--- script.js
+++ script.js
@@ -7,6 +7,22 @@ paragraphTwo.textContent = "Developers for life!";
 let img = document.createElement('img');
 img.src = "http://static.minitokyo.net/downloads/17/39/54467.jpg";
 img.alt = "Rurouni Kenshin";
+
 let container = document.querySelector('.img-container');
 container.appendChild(img);
 
+// let paragraphs = document.querySelectorAll('.para');
+// paragraphs.style.color = 'red';
+
+// paragraphOne.style.font-size = '40px';
+
+//Add button
+let button = document.createElement('button');
+button.textContent = "Button";
+
+container.appendChild(button);
+
+//onclick, hide image
+button.addEventListener('click', () => {
+    img.style.display = 'none';
+});
